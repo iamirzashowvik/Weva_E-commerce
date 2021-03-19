97ffb1477eaff6eeeee367c0b4d1d00f66606217
iamirzashowvik <showvikmirza@gmail.com>
2021-03-19 22:05:59 +0600

    adding files

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:weva/screens/localization.dart';
+
+import 'package:weva/screens/user_profile.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Weva App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Localization(),
+    );
+  }
+}
